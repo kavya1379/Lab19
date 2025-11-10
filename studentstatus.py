f907da6145b1eff6b4b9d0fbe0afdb27c62bb245
kavya1379 <horkerikavya@gmail.com>
2025-11-10 12:28:36 +0530

    Create studentstatus.py

---
+++ studentstatus.py
@@ -0,0 +1,12 @@
+import sys
+if len(sys.argv) != 3:
+  print("Usage: python student.py <name><roll>");
+  sys.exit(1)
+  
+script_name=sys.argv[0]
+name = sys.argv[1]
+rollno = sys.argv[2]
+
+print("Scipt Name:"script_name)
+print("Student Name:"name)
+print("Roll Number:"rollno)
